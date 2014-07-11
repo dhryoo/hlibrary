c1dbdcb02bd6d80699796b2068451d5de2d26968
Doohyun Ryoo <idfeelme@gmail.com>
2014-07-11 15:48:25 +0900

    book complete

--- app/scripts/services/postdata.js
+++ app/scripts/services/postdata.js
@@ -14,7 +14,7 @@ angular.module('hl4App')
     var baseURL = 'http://www.hlibrary.kr/press/?callback=JSON_CALLBACK&';
 
     var teacherListJSON = function () {
-        var param = 'json=get_category_posts&category_id=2&include=attachments,title,tags,custom_fields&custom_filelds=teacher_name,teacher_picture,teacher_introduce,teacher_profile,teacher_portfolio_count';
+        var param = 'json=get_category_posts&category_id=2&include=attachments,title,tags,comment_count,custom_fields&custom_filelds=teacher_name,teacher_picture,teacher_introduce,teacher_profile,teacher_portfolio_count';
 
         var newURL = baseURL + param;
 
@@ -31,7 +31,7 @@ angular.module('hl4App')
     };
 
      var bookListJSON = function () {
-        var param = 'json=get_category_posts&category_id=6&include=attachments,title,custom_fields&custom_filelds=book_url,book_image';
+        var param = 'json=get_category_posts&category_id=6&include=attachments,title,comment_count,custom_fields&custom_filelds=book_url,book_image';
         var newURL = baseURL + param;
         console.log(newURL);
         var deferred = $q.defer();
